214c911ea53aa986c8719e3303d1de7406f99dea
Deekshitha <98965988+MDeekshithaReddy@users.noreply.github.com>
2025-04-20 12:12:51 +0530

    Update a2c_train_combined.py

--- rl_agents/a2c_train_combined.py
+++ rl_agents/a2c_train_combined.py
@@ -8,7 +8,7 @@ from stable_baselines3.common.env_util import make_vec_env
 
 from finrl.config import INDICATORS, TRAINED_MODEL_DIR
 from finrl.main import check_and_make_directories
-from utils.env import StockTradingEnv
+from utils.env_combined import StockTradingEnv
 
 
 
